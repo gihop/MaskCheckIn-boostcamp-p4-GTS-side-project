41a20d6d7529beb7221312f3a104bfe0f9876935
gihop <giho-p@hanmail.net>
2021-06-17 15:01:09 +0900

    <fix>: Modify AndroidManifest.xml

--- app/build/generated/source/buildConfig/splits/com/google/mlkit/vision/demo/BuildConfig.java
+++ app/build/generated/source/buildConfig/splits/com/google/mlkit/vision/demo/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.jihopark.mlkit.vision.demo";
   public static final String BUILD_TYPE = "splits";
-  public static final int VERSION_CODE = 12;
+  public static final int VERSION_CODE = 6;
   public static final String VERSION_NAME = "1.0";
 }
